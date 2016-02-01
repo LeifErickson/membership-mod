f46c87aa8f4afa97b3a99402ed7c89f4d5610a10
bynnjessa <bynnjessa@gmail.com>
2016-02-01 17:12:29 +0800

    fixed coding structure for psr-2

--- src/member.php
+++ src/member.php
@@ -8,7 +8,7 @@ use Education;
 use SeminarsTrainings;
 use HighestPosition;
 
-class Member //\ Member extends Person*
+class Member extends Person
 {
     public $membershipstatus;
     public $company;
@@ -18,63 +18,75 @@ class Member //\ Member extends Person*
     public $JCIsenatorialno;
     public $dateofinduction;
 
-    public function setMemstatus($membershipstatus)
+    public function setMemStatus($membershipstatus)
     {
-        $this -> membershipstatus = $membershipstatus;
-    }
-    public function setCompany($company)
-    {
-        $this -> company = $company;
-    }
-    public function setCompanyAddress($companyaddress)
-    {
-	$this -> companyaddress = $companyaddress;
-    }
-    public function setProfession($profession)
-    {
-	$this -> profession = $profession;
-    }
-    public function setStatus($status)
-    {
-	$this -> status = $status;
-    }
-    public function setJCISenatorialNo($JCIsenatorialno)
-    {
-	$this -> JCIsenatorialno = $JCIsenatorialno;
-    }
-    public function setDateofIndution($dateofinduction)
-    {
-	$this -> dateofinduction = $dateofinduction;
-    }
-    public function getMemstatus()
-    {
-	return $this -> membershipstatus;
-    }
-    public function getCompany()
-    {
-	return $this -> company;
-    }
-    public function getCompanyAddress()
-    {
-	return $this -> companyaddress;
-    }
-    public function getProfession()
-    {
-	return $this -> profession;
-    }
-    public function getStatus()
-    {
-	return $this -> status;
-    }
-    public function getJCISenatorialNo()
-    {
-	return $this -> JCIsenatorialno;
-    }
-    public function getDateofInduction()
-    {
-	return $this -> dateofinduction;
+    	$this->membershipstatus = $membershipstatus;
     }
 
+    public function setCompany($company)
+    {
+    	$this->company = $company;
+    }
+
+    public function setCompanyAddress($companyaddress)
+    {
+		$this->companyaddress = $companyaddress;
+    }
+
+    public function setProfession($profession)
+    {
+		$this->profession = $profession;
+    }
+
+    public function setStatus($status)
+    {
+		$this->status = $status;
+    }
+
+    public function setJCISenatorialNo($JCIsenatorialno)
+    {
+		$this->JCIsenatorialno = $JCIsenatorialno;
+    }
+
+    public function setDateofIndution($dateofinduction)
+    {
+		$this->dateofinduction = $dateofinduction;
+    }
+
+    public function getMemstatus()
+    {
+		return $this->membershipstatus;
+    }
+
+    public function getCompany()
+    {
+		return $this->company;
+    }
+
+    public function getCompanyAddress()
+    {
+		return $this->companyaddress;
+    }
+
+    public function getProfession()
+    {
+		return $this->profession;
+    }
+
+    public function getStatus()
+    {
+		return $this->status;
+    }
+
+    public function getJCISenatorialNo()
+    {
+		return $this->JCIsenatorialno;
+    }
+
+    public function getDateofInduction()
+    {
+		return $this->dateofinduction;
+    }
 
 }
 
